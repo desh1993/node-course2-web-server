fe7248113ce7f5e6667f3485a1bacd4112296817
Desh <szemingrage@gmail.com>
2018-08-23 15:00:31 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const maintenance = require('maintenance');
+
+let app = express();
+app.set('view engine','hbs');
+app.use( (req, res, next) => {
+    let now = new Date();
+    let log = `time : ${now} : ${req.method} ${req.url}` ;
+    console.log( log );
+    fs.appendFile('server.log',log + '\n' ,(err) =>{
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    
+     next();
+  });
+
+  //maintenance 
+  /*
+  app.use((req,res,next)=>{
+      console.log(req.url);
+      res.render('maintenance.hbs',{
+                Body : 'Maintenance',
+                Author : 'Desh',
+                Reason : 'Site being updated at the moment'
+            });
+     // res.send(200);
+  })
+*/
+//   maintenance(app,true);
+ 
+//register partials
+hbs.registerPartials(__dirname+ "/views/partials");
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+})
+//capitalization
+hbs.registerHelper('screamIt',(str)=>{
+    return str.toUpperCase();
+})
+
+
+
+//help.html
+app.use(express.static(__dirname + "/public"));
+// app.get('/',(req,res)=>{
+//     //res.send(`<h1>Hello Express !</h1>`);
+
+//     res.send({
+//         name:'Desh',
+//         likes : ['Boxing','Brazilian Jiu-Jitsu','Kickboxing']
+//     });
+// });
+
+app.get('/',(req,res)=>{
+    res.render('welcome.hbs',{
+        Title : 'Home page',
+        welcomeText : `Welcome to Our page`,
+        Author : 'Desh'
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        Title : 'About Us',
+        Author : 'Desh'
+    });
+});
+// maintenance
+// app.get('/maintenance',(req,res)=>{
+//     res.render('maintenance.hbs',{
+//         Body : 'Maintenance',
+//         Author : 'Desh'
+//     });
+// });
+// /bad - send back json with errorMessage 
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage : `Bad request`
+    });
+   
+});
+
+
+app.listen(3000,()=>{
+    console.log(`Server is up on port 3000`);
+});
+
+
